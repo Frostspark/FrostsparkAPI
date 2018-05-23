345eba95d0545393c8449d928f872643e60633b1
quake1337 <3310937+bartico6@users.noreply.github.com>
2018-05-24 00:40:29 +0200

    Add IEvent

---
+++ FrostsparkAPI/Events/IEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events
+{
+    public abstract class IEvent
+    {
+        /// <summary>
+        /// Returns whether or not this event can be cancelled.
+        /// </summary>
+        public abstract bool Cancellable { get; }
+
+        /// <summary>
+        /// Returns whether or not the event is cancelled, or sets its cancellation state.
+        /// </summary>
+        public abstract bool IsCancelled { get; set; }
+
+    }
+}
