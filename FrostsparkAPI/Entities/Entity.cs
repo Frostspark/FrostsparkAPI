f3310c8a27d378cf3fa994c95c353ec3802f625e
quake1337 <3310937+bartico6@users.noreply.github.com>
2020-03-24 00:36:27 +0100

    Create Vector2D & expose velocity & position for entities.

--- FrostsparkAPI/Entities/Entity.cs
+++ FrostsparkAPI/Entities/Entity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Frostspark.API.Utilities;
 
 namespace Frostspark.API.Entities
 {
@@ -14,8 +15,19 @@ namespace Frostspark.API.Entities
         public abstract void Kill();
 
         /// <summary>
-        /// Returns this entity's name.
+        /// This entity's name.
         /// </summary>
         public abstract string Name { get; }
+
+        /// <summary>
+        /// This entity's position in the world.
+        /// </summary>
+        public Vector2D<float> Position { get; }
+
+        /// <summary>
+        /// This entity's velocity.
+        /// </summary>
+        public Vector2D<float> Velocity { get; }
+
     }
 }
---
+++ FrostsparkAPI/Utilities/Vector2D.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Utilities
+{
+    public struct Vector2D<T> : IEquatable<Vector2D<T>>
+    {
+        public readonly T X;
+        public readonly T Y;
+
+        public Vector2D(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vector2D<T> d && Equals(d);
+        }
+
+        public bool Equals(Vector2D<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(X, other.X) &&
+                   EqualityComparer<T>.Default.Equals(Y, other.Y);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T>.Default.GetHashCode(X);
+            hashCode = hashCode * -1521134295 + EqualityComparer<T>.Default.GetHashCode(Y);
+            return hashCode;
+        }
+
+        public static bool operator ==(Vector2D<T> left, Vector2D<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector2D<T> left, Vector2D<T> right)
+        {
+            return !(left == right);
+        }
+    }
+}
