497f60b5662fb6ebb62ca38b051269e0a0c9217d
quake1337 <3310937+bartico6@users.noreply.github.com>
2020-05-01 04:27:33 +0200

    Add IEquatable to Color, make the fields readonly.

--- FrostsparkAPI/Utilities/Color.cs
+++ FrostsparkAPI/Utilities/Color.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Frostspark.API.Utilities
 {
-    public struct Color
+    public struct Color : IEquatable<Color>
     {
         public Color(byte r, byte g, byte b)
         {
@@ -15,8 +15,39 @@ namespace Frostspark.API.Utilities
             B = b;
         }
 
-        public byte R;
-        public byte G;
-        public byte B;
+        public readonly byte R;
+        public readonly byte G;
+        public readonly byte B;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Color color && Equals(color);
+        }
+
+        public bool Equals(Color other)
+        {
+            return R == other.R &&
+                   G == other.G &&
+                   B == other.B;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1520100960;
+            hashCode = hashCode * -1521134295 + R.GetHashCode();
+            hashCode = hashCode * -1521134295 + G.GetHashCode();
+            hashCode = hashCode * -1521134295 + B.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Color left, Color right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Color left, Color right)
+        {
+            return !(left == right);
+        }
     }
 }
