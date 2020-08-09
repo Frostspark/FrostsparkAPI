f3285bd70b13e528307fbab61372456c86727c45
quake1337 <3310937+bartico6@users.noreply.github.com>
2020-08-09 18:45:13 +0200

    Add chat event

---
+++ Frostspark.API/Events/Players/PlayerChatEvent.cs
@@ -0,0 +1,38 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frostspark.API.Events.Players
+{
+    public class PlayerChatEvent : PlayerEvent, ICancellable
+    {
+        public PlayerChatEvent(Player player, API.Server server) : base(player, server)
+        {
+        }
+
+        /// <summary>
+        /// The message the player sent.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The chat format string the message will use.
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// The base color of the chat message
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// A list of players that will receive this message.
+        /// </summary>
+        public IEnumerable<Player> Recipients { get; set; }
+
+        public bool Cancelled { get; set; }
+    }
+}
