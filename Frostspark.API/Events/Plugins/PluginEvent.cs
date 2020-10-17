730a8c4938f88f6e6eeb2673da8cafb82571455a
quake1337 <3310937+bartico6@users.noreply.github.com>
2020-10-18 00:41:17 +0200

    Add Plugin-related events

---
+++ Frostspark.API/Events/Plugins/PluginEvent.cs
@@ -0,0 +1,21 @@
+﻿using Frostspark.API.Plugins;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frostspark.API.Events.Plugins
+{
+    public class PluginEvent : Event
+    {
+        public PluginEvent(Plugin plugin, API.Server server) : base(server)
+        {
+            Plugin = plugin;
+        }
+
+        /// <summary>
+        /// The plugin involved with this event.
+        /// </summary>
+        public Plugin Plugin { get; }
+    }
+}
---
+++ Frostspark.API/Events/Plugins/PluginLoadEvent.cs
@@ -0,0 +1,15 @@
+﻿using Frostspark.API.Plugins;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frostspark.API.Events.Plugins
+{
+    public class PluginLoadEvent : PluginEvent
+    {
+        public PluginLoadEvent(Plugin plugin, API.Server server) : base(plugin, server)
+        {
+        }
+    }
+}
