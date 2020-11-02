7b14925fc366702e5f984b6d0131bc29a83b6ede
quake1337 <3310937+bartico6@users.noreply.github.com>
2020-11-02 12:56:07 +0100

    Add PlayerCommandEvent.

---
+++ Frostspark.API/Events/Players/PlayerCommandEvent.cs
@@ -0,0 +1,23 @@
+﻿using Frostspark.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.Players
+{
+    public class PlayerCommandEvent : PlayerEvent, ICancellable
+    {
+        public PlayerCommandEvent(Player player, API.Server server) : base(player, server)
+        {
+        }
+
+        /// <summary>
+        /// The command the player has issued, without the prefix.
+        /// </summary>
+        public string Command { get; set; }
+
+        public bool Cancelled { get; set; }
+    }
+}
