9d3b7b332e0ee2f9d3229d9044f10a1b58cee40d
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-01-10 23:23:34 +0100

    Update EventHandler model

--- Frostspark.API/Events/EventHandler.cs
+++ Frostspark.API/Events/EventHandler.cs
@@ -1,11 +1,32 @@
-﻿namespace Frostspark.API.Events
+﻿using Frostspark.API.Events.Interfaces;
+
+using System;
+
+namespace Frostspark.API.Events
 {
-    public abstract class EventHandler<T> where T : Event
+    public abstract class EventHandler<T> : EventHandler where T : Event
+    {
+        public override Type HandlerType => typeof(T);
+
+        public sealed override void HandleEvent(Event e)
+        {
+            if (e is T t)
+            {
+                Handle(t);
+            }
+        }
+
+        public abstract void Handle(T obj);
+    }
+
+    public abstract class EventHandler
     {
         public virtual bool AllowCanceled => true;
 
         public virtual int Priority => 0;
 
-        public abstract void Handle(T obj);
+        public abstract Type HandlerType { get; }
+
+        public abstract void HandleEvent(Event e);
     }
 }
