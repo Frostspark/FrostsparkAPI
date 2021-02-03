4c37291ab75101f69af4813c3c5c8a6e1bcd61fb
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-02-03 11:27:56 +0100

    Add Watchdog shutdown event

---
+++ Frostspark.API/Events/Server/ServerWatchdogEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.Server
+{
+    public class ServerWatchdogEvent : Event
+    {
+        public ServerWatchdogEvent(API.Server server) : base(server)
+        {
+        }
+    }
+}
