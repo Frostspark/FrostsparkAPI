3d1fdc3ce1af591b2bf2cfb404521aac04d72a05
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-02-03 12:47:19 +0100

    Mark watchdog event as async

--- Frostspark.API/Events/Server/ServerWatchdogEvent.cs
+++ Frostspark.API/Events/Server/ServerWatchdogEvent.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Frostspark.API.Events.Interfaces;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Frostspark.API.Events.Server
 {
-    public class ServerWatchdogEvent : Event
+    public class ServerWatchdogEvent : Event, IAsynchronous
     {
         public ServerWatchdogEvent(API.Server server) : base(server)
         {
