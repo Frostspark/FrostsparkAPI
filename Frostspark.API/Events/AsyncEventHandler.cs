3d90f8536e5a0a593b641e63aa8da4d4c0643b86
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-04-28 18:29:03 +0200

    DropAwait support in async EventHandlers

--- Frostspark.API/Events/AsyncEventHandler.cs
+++ Frostspark.API/Events/AsyncEventHandler.cs
@@ -32,5 +32,11 @@ namespace Frostspark.API.Events
         /// <para>Only set this if you're **ABSOLUTELY CERTAIN** you know what you're doing, and have considered the consequences of your await continuation running long after the event invoker checked the result.</para>
         /// </summary>
         public virtual bool SuppressSyncInvokeWarning { get; } = false;
+
+        /// <summary>
+        /// If set to true, async EventManager calls will not await this handler.
+        /// <para>Use this if this handler *doesn't* need to modify the event state (and thus can afford to not be awaited) - for instance, logging.</para>
+        /// </summary>
+        public virtual bool DropAwait { get; } = false;
     }
 }
