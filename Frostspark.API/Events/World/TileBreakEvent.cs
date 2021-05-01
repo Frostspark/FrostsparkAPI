e3a6c396c6660ef0131a7d85d9005a9f7a2e548c
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-05-01 18:27:33 +0200

    [WIP] Add TileBreakEvent

---
+++ Frostspark.API/Events/World/TileBreakEvent.cs
@@ -0,0 +1,26 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Events.Interfaces;
+using Frostspark.API.Worlds;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.World
+{
+    public class TileBreakEvent : TileEvent, IHasSource<Player>, ICancellable
+    {
+        public TileBreakEvent(Player player, Tile tile, Worlds.World world, API.Server server) : base(tile, world, server)
+        {
+            Player = player;
+        }
+
+        public Player Player { get; }
+
+        public bool Cancelled { get; set; }
+
+        Player IHasSource<Player>.Source => Player;
+    }
+}
