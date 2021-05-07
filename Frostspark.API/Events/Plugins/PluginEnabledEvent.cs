47b26297c51f97ebd64af5c87cf184972a479bd6
quake1337 <3310937+bartico6@users.noreply.github.com>
2021-05-07 21:20:06 +0200

    Rename Plugin-related events to past tense

---
+++ Frostspark.API/Events/Plugins/PluginEnabledEvent.cs
@@ -0,0 +1,11 @@
+﻿using Frostspark.API.Plugins;
+
+namespace Frostspark.API.Events.Plugins
+{
+    public class PluginEnabledEvent : PluginEvent
+    {
+        public PluginEnabledEvent(Plugin plugin, API.Server server) : base(plugin, server)
+        {
+        }
+    }
+}
---
+++ Frostspark.API/Events/Plugins/PluginLoadedEvent.cs
@@ -0,0 +1,11 @@
+﻿using Frostspark.API.Plugins;
+
+namespace Frostspark.API.Events.Plugins
+{
+    public class PluginLoadedEvent : PluginEvent
+    {
+        public PluginLoadedEvent(Plugin plugin, API.Server server) : base(plugin, server)
+        {
+        }
+    }
+}
