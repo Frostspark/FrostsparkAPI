1b8a6e303662a120bf3e106dbf86b84616907210
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-01-24 00:04:01 +0100

    Add Ping event

---
+++ Frostspark.API/Events/Players/PlayerPingMinimapEvent.cs
@@ -0,0 +1,23 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Events.Interfaces;
+using Frostspark.API.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.Players
+{
+    public class PlayerPingMinimapEvent : PlayerEvent, ICancellable
+    {
+        public PlayerPingMinimapEvent(Player player, API.Server server) : base(player, server)
+        {
+        }
+
+        public Vector2D<int> Position { get; set; }
+
+        public bool Cancelled { get; set; }
+    }
+}
