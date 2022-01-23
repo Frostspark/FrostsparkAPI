1924304bad29a1de06f150abb5f2e64b64249496
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-01-23 23:14:23 +0100

    Add DifficultyMismatch event

---
+++ Frostspark.API/Events/Players/PlayerDifficultyMismatchEvent.cs
@@ -0,0 +1,26 @@
+﻿using Frostspark.API.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.Players
+{
+    internal class PlayerDifficultyMismatchEvent : PlayerEvent
+    {
+        public PlayerDifficultyMismatchEvent(Player player, API.Server server) : base(player, server)
+        {
+        }
+
+        public DifficultyResult Result { get; set; }
+
+        public enum DifficultyResult
+        {
+            Kick,
+            SwitchDifficulty,
+            Allow
+        }
+    }
+}
