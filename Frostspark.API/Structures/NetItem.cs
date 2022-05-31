755a8f7062b695f97a9780ce53e5d872c805a884
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-05-31 16:15:56 +0200

    Add NetItem.
    - Simple 3-field structure detailing an item's most important
      characteristics as networked by the game.

---
+++ Frostspark.API/Structures/NetItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Structures
+{
+    public struct NetItem
+    {
+        public int Type;
+        public int Stack;
+        public byte Prefix;
+    }
+}
