8cf185c5fbecd0c54b60c94d7333cc6f50a2df77
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-10-14 20:23:22 +0200

    Add Clentaminator events

---
+++ Frostspark.API/Events/World/Players/PlayerWallConvertBiomeEvent.cs
@@ -0,0 +1,22 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Events.Interfaces;
+using Frostspark.API.Worlds;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.World.Players
+{
+    public sealed class PlayerWallConvertBiomeEvent : WallConvertBiomeEvent, IHasSource<Player>
+    {
+        public PlayerWallConvertBiomeEvent(Player player, int type, Tile tile, Worlds.World world, API.Server server) : base(type, tile, world, server)
+        {
+            Source = player;
+        }
+
+        public Player Source { get; }
+    }
+}
---
+++ Frostspark.API/Events/World/WallConvertBiomeEvent.cs
@@ -0,0 +1,23 @@
+﻿using Frostspark.API.Events.Interfaces;
+using Frostspark.API.Worlds;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.World
+{
+    public abstract class WallConvertBiomeEvent : SingleTileEvent, ICancellable
+    {
+        public WallConvertBiomeEvent(int type, Tile tile, Worlds.World world, API.Server server) : base(tile, world, server)
+        {
+            Type = type;
+        }
+
+        public bool Cancelled { get; set; }
+
+        public int Type { get; set; }
+    }
+}
