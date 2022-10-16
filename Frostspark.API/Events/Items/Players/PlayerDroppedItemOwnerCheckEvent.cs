91d3b39f2cc9d37003f75e2ebb450f22ecc63e32
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-10-16 21:15:49 +0200

    Item ownership check event

---
+++ Frostspark.API/Events/Items/Players/PlayerDroppedItemOwnerCheckEvent.cs
@@ -0,0 +1,26 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Events.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.Items.Players
+{
+    public sealed class PlayerDroppedItemOwnerCheckEvent : DroppedItemEvent, ICancellable, IHasSource<Player>
+    {
+        public PlayerDroppedItemOwnerCheckEvent(Player player, DroppedItem item, API.Server server) : base(item, server)
+        {
+            Source = player;
+        }
+
+        public Player Source { get; }
+
+        /// <summary>
+        /// Cancelling this event will exclude this player from being the item owner and having the ability to pick it up.
+        /// </summary>
+        public bool Cancelled { get; set; }
+    }
+}
