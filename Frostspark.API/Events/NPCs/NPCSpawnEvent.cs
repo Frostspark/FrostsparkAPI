6e62a0751c7730d576a07a943de4c4d850277de1
quake1337 <3310937+bartico6@users.noreply.github.com>
2022-10-17 17:55:11 +0200

    NPCSpawnEvent

---
+++ Frostspark.API/Events/NPCs/NPCSpawnEvent.cs
@@ -0,0 +1,20 @@
+﻿using Frostspark.API.Entities;
+using Frostspark.API.Events.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frostspark.API.Events.NPCs
+{
+    public sealed class NPCSpawnEvent : NPCEvent, ICancellable
+    {
+        public NPCSpawnEvent(NPC npc, API.Server server) : base(npc, server)
+        {
+        }
+
+        public bool Cancelled { get; set; }
+    }
+}
