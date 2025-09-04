ea41b262f74bbbac3bbb10475613c911c1762424
quake1337 <3310937+bartico6@users.noreply.github.com>
2025-09-04 15:52:40 +0200

    Add ServerShutdownEvent

---
+++ Frostspark.API/Events/Server/ServerShutdownEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Frostspark.API.Events.Server
+{
+    public class ServerShutdownEvent : Event
+    {
+        public ServerShutdownEvent(API.Server server) : base(server)
+        {
+        }
+    }
+}
